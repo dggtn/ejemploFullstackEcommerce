50b35523a0fcb380258ded61e7ef123374e03ef1
Daniela <dggtn97@gmail.com>
2025-03-23 16:20:29 -0300

    agrego spinner como componente para las secciones que no cuentan con productos

---
+++ src/frontend/frontendEcommerce/src/component/Spinner.jsx
@@ -0,0 +1,11 @@
+export default function Spinner() {
+
+    return (
+        <div className="bg-white " >
+            <img src="/public/notFound.jpg"></img>
+            
+        </div>
+    )
+}
+
+
