de1f2acc1e2a33b7d565c34202e1e67ff23ea50a
Daniela <dggtn97@gmail.com>
2025-03-23 13:31:20 -0300

    agrego en app la ruta para el formulario

--- src/frontend/frontendEcommerce/src/App.jsx
+++ src/frontend/frontendEcommerce/src/App.jsx
@@ -4,16 +4,24 @@ import NavBar from './component/NavBar'
 import './tailwind.css'
 import Detalleproducto from './component/DetalleProducto'
 import Home from './component/Home'
+import Footer  from './component/Footer'
+import Formulario from './component/Formulario'
+
 function App() {
   return (
+    <>
     <BrowserRouter>
       <NavBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/categoria/:id" element={<Catalogo />} />
         <Route exact path="/detalle/:id" element={<Detalleproducto />} />
+        <Route exact path="/formulario" element={<Formulario />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
+    
+    </>
   )
 }
 
