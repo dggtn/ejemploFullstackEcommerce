fdebd1cbe0e46d2cb01d9200497702e731a9d53b
Daniela <dggtn97@gmail.com>
2025-03-23 16:18:36 -0300

    agrego seccion perfil

---
+++ src/frontend/frontendEcommerce/src/component/Licencia.jsx
@@ -0,0 +1,112 @@
+import { useEffect } from "react"
+export default function Licencia() {
+        useEffect(() => {
+            window.scrollTo(0, 0)
+        }, [])
+        return (
+            <section className="relative bg-lightgreen-900 text-bg-green py-20 bg-cover  bg-white">
+                <div className="container mx-auto align-text-top px-4 md:px-8">
+                    <h1 className="mt-10 text-4xl md:text-5xl font-extrabold mb-6 text-dark">
+                        Términos y Condiciones</h1>
+
+                    <h2> Declaración de Privacidad</h2>
+
+                    <h3>SECCIÓN 1 – ¿QUÉ HACEMOS CON TU INFORMACIÓN?</h3>
+
+                    <p>Cuando compras algo en nuestra tienda, como parte del proceso de compra-venta, recopilamos la información personal que nos proporcionas, como tu nombre, dirección y correo electrónico.
+
+                        Cuando navegas por nuestra tienda, también recibimos automáticamente la dirección de protocolo de internet (IP) de tu computadora para proporcionarnos información que nos ayuda a conocer tu navegador y sistema operativo.
+
+                        Marketing por correo electrónico: Solo con tu permiso, podemos enviarte correos electrónicos acerca de nuestra tienda, nuevos productos, recetas, promociones y otras actualizaciones.
+                    </p>
+                    <h3>SECCIÓN 2 – CONSENTIMIENTO</h3>
+
+                    <p>¿Cómo obtienen mi consentimiento?
+
+                        Cuando nos proporcionas tu información personal para completar una transacción, verificar tu tarjeta de crédito, realizar un pedido, organizar una entrega o devolver una compra, implicamos que consientes la recopilación y el uso de esta información únicamente para ese propósito específico.
+
+                        Si pedimos tu información personal para un motivo secundario, como marketing, te solicitaremos directamente tu consentimiento expreso o te daremos la oportunidad de decir que no.
+
+                        ¿Cómo retiro mi consentimiento?
+
+                        Si después de optar por participar cambias de opinión, puedes retirar tu consentimiento para que te contactemos, para la recopilación, uso o divulgación continua de tu información en cualquier momento, contactándonos a hello@consciencedietetica.com.ar o “dándote de baja” de las actualizaciones por correo electrónico en tu correo.
+                    </p>
+                    <h3>SECCIÓN 3 – DIVULGACIÓN</h3>
+
+                    <p>Podemos divulgar tu información personal si así lo exigen las leyes o si violas nuestros Términos de Servicio.
+
+                        <h3>Pago:</h3>
+
+                        <p>En Dietetica Consciente utilizamos una plataforma de pago seguro de terceros llamada Stripe, para procesar todas las transacciones con tarjeta de crédito y débito.
+
+                            Todos los métodos de pago directos cumplen con los estándares establecidos por PCI-DSS, gestionados por el PCI Security Standards Council, un esfuerzo conjunto de marcas como Visa, MasterCard, American Express y Discover. Aceptamos únicamente Visa o Mastercard.
+
+                            Los requisitos de PCI-DSS ayudan a garantizar el manejo seguro de la información de tarjetas de crédito en nuestra tienda y sus proveedores de servicios.</p>
+                    </p>
+                    <h3>SECCIÓN 4 – SERVICIOS DE TERCEROS</h3>
+
+                    <p>En general, los proveedores de terceros que utilizamos solo recopilarán, usarán y divulgarán tu información en la medida necesaria para permitirles realizar los servicios que nos proporcionan.
+
+                        Sin embargo, ciertos proveedores de servicios de terceros, como los gateways de pago y otros procesadores de transacciones, tienen sus propias políticas de privacidad con respecto a la información que debemos proporcionarles para las transacciones relacionadas con tus compras.
+
+                        Recomendamos que leas sus políticas de privacidad para entender cómo se manejará tu información personal por parte de estos proveedores.
+
+                        Recuerda que ciertos proveedores pueden estar ubicados en una jurisdicción diferente a la nuestra, por lo que si decides proceder con una transacción que involucre los servicios de un proveedor de terceros, tu información podrá estar sujeta a las leyes de la jurisdicción en la que ese proveedor o sus instalaciones se encuentren.
+
+                        Una vez que dejes nuestro sitio web o seas redirigido a un sitio web o aplicación de terceros, ya no estarás regido por nuestra Política de Privacidad ni por las políticas de nuestro sitio.
+
+                        Enlaces
+
+                        Cuando haces clic en enlaces dentro de nuestra tienda, puedes ser dirigido fuera de nuestro sitio. No nos responsabilizamos por las prácticas de privacidad de otros sitios y te animamos a leer sus declaraciones de privacidad.</p>
+
+                    <h3>SECCIÓN 5 – SEGURIDAD</h3>
+
+                    <p>Para proteger tu información personal, tomamos precauciones razonables y seguimos las mejores prácticas de la industria para asegurarnos de que no se pierda, se haga un uso indebido, se acceda, se divulgue, se altere ni se destruya inapropiadamente.
+
+                        Si nos proporcionas tu información de tarjeta de crédito, esta se encripta usando tecnología de capa de sockets seguros (SSL) y se almacena con encriptación AES-256. Aunque ningún método de transmisión por internet o almacenamiento electrónico es 100% seguro, seguimos todos los requisitos PCI-DSS e implementamos estándares adicionales generalmente aceptados por la industria.
+
+                        SECCIÓN 6 – CAMBIOS A ESTA POLÍTICA DE PRIVACIDAD
+
+                        Nos reservamos el derecho de modificar esta política de privacidad en cualquier momento, por lo que te recomendamos revisarla frecuentemente. Los cambios y aclaraciones entrarán en vigencia de inmediato una vez que se publiquen en el sitio web.
+
+                        Si nuestra tienda es adquirida o fusionada con otra empresa, tu información puede ser transferida a los nuevos propietarios para que podamos seguir vendiéndote productos.</p>
+
+                    <h3>PREGUNTAS Y CONTACTO</h3>
+
+                    <p>Si tienes alguna pregunta o deseas modificar o eliminar cualquier información personal que tengamos sobre ti, presentar una queja o simplemente deseas más información, puedes contactar a nuestro Director de Información:
+
+                        Juan Pérez a hello@consciencedietetica.com.ar o por correo a:
+
+                        Dietetica Consciente
+
+                        Calle Ficticia 123
+
+                        Buenos Aires, Argentina</p>
+
+                    —
+
+                    <h3>TÉRMINOS DE SERVICIO</h3>
+
+
+
+                    <p>Este sitio web es operado por Dietetica Consciente. A lo largo del sitio, los términos "nosotros", "nos" y "nuestro" se refieren a Dietetica Consciente.
+
+                        Dietetica Consciente ofrece este sitio web, incluyendo toda la información, herramientas y servicios disponibles en este sitio, condicionados a tu aceptación de todos los términos, condiciones, políticas y avisos aquí expuestos.
+
+                        Al visitar nuestro sitio y/o realizar una compra/suscripción con nosotros, te comprometes con nuestro "Servicio" y aceptas estar sujeto a los siguientes términos y condiciones ("Términos de Servicio", "Términos"), incluidos los términos y condiciones adicionales y políticas referenciadas en este documento y/o disponibles mediante enlace.
+
+                        Por favor, lee estos Términos de Servicio cuidadosamente antes de acceder o usar nuestro sitio web.</p>
+
+                    <h3>SECCIÓN 1 TÉRMINOS DE LA TIENDA EN LÍNEA</h3>
+
+                    <p>Al aceptar estos Términos de Servicio, declaras que tienes al menos 18 años de edad o que has obtenido el consentimiento adecuado para usar este sitio.
+
+                        No podrás usar nuestros productos para ningún propósito ilegal o no autorizado, ni violar ninguna ley en tu jurisdicción.
+
+                        No debes transmitir ningún tipo de virus o código destructivo.
+
+                        El incumplimiento de cualquier término resultará en la terminación inmediata de tus servicios.</p>
+                </div>
+            </section>
+        )
+    }
