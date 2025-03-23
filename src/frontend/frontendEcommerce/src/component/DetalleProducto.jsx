ca14fa59e79349285a86ebfca9ceab009fa27f24
Daniela <dggtn97@gmail.com>
2025-03-23 16:13:44 -0300

    agrego componente detalleProducto

--- src/frontend/frontendEcommerce/src/component/DetalleProducto.jsx
+++ src/frontend/frontendEcommerce/src/component/DetalleProducto.jsx
@@ -9,16 +9,16 @@ export default function Detalleproducto() {
         }, [params]);
     
     return (
-        <div class="bg-white">
-        <div class="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+        <div className="bg-white">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
           <div>
-            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Detalles de {producto.nombre}:</h2>
-            <p class="mt-4 text-gray-500">{producto.nombre}es organico.</p>
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Detalles de {producto.nombre}:</h2>
+            <p className="mt-4 text-gray-500">{producto.nombre}es organico.</p>
       
-            <dl class="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
-              <div class="border-t border-gray-200 pt-4">
-                <dt class="font-medium text-gray-900">Origen</dt>
-                <dd class="mt-2 text-sm text-gray-500">Producido por:</dd>
+            <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+              <div className="border-t border-gray-200 pt-4">
+                <dt className="font-medium text-gray-900">Origen</dt>
+                <dd className="mt-2 text-sm text-gray-500">Producido por:</dd>
               </div>
             </dl>
           </div>
