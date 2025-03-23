5117b7725df87289cc853b119e058da4ad71d6d4
Daniela <dggtn97@gmail.com>
2025-03-23 13:31:53 -0300

    agrego componente catalogo

--- src/frontend/frontendEcommerce/src/component/Catalogo.jsx
+++ src/frontend/frontendEcommerce/src/component/Catalogo.jsx
@@ -15,11 +15,13 @@ export default function Catalogo() {
         <div className="bg-white">
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                 <h2 className="sr-only">Productos</h2>
+                <div class="mt-4 flex justify-between">
                 {
                     productos.map((producto) => (
                         <Producto nombre={producto.nombre} precio={producto.precio} imagen={producto.imagen} key={producto.id}/>
                     ))
                 }
+            </div>
             </div>
         </div>
     )
