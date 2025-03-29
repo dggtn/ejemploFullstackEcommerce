c9ecb2318d383d32a2933068c89dc0103428ec6b
Daniela <dggtn97@gmail.com>
2025-03-29 19:39:20 -0300

    hago que la interfaz de IproductoRepositor extienda de JpaREpository

--- src/main/java/ar/edu/uade/ecommerce/repository/IproductosRepository.java
+++ src/main/java/ar/edu/uade/ecommerce/repository/IproductosRepository.java
@@ -1,104 +1,19 @@
 package ar.edu.uade.ecommerce.repository;
 
 
+import ar.edu.uade.ecommerce.entity.Category;
 import ar.edu.uade.ecommerce.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
-import java.util.LinkedList;
 import java.util.List;
 
 @Repository
-public class RepositorioProductosEnMemoria {
-    private List<Producto> productos;
+public interface IproductosRepository extends JpaRepository<Category, Long> {
 
-    public RepositorioProductosEnMemoria() {
-        this.productos = new LinkedList<>();
-        Producto almendras = new Producto();
-        Producto nuez = new Producto();
-        Producto pistachio = new Producto();
-        almendras.setNombre("almendra");
-        nuez.setNombre("nuez");
-        pistachio.setNombre("pistachio");
+    public List<Producto> obtenerTodos();
 
-        almendras.setCategoria("frutas_secas");
-        nuez.setCategoria("frutas_secas");
-        pistachio.setCategoria("frutas_secas");
+    public void guardar(Producto producto);
 
-        almendras.setCantidad(100);
-        nuez.setCantidad(100);
-        pistachio.setCantidad(100);
-
-
-        almendras.setPrecio(new BigDecimal("150.00"));
-        nuez.setPrecio(new BigDecimal("300.00"));
-        pistachio.setPrecio(new BigDecimal("200.00"));
-
-        almendras.setId(1);
-        nuez.setId(2);
-        pistachio.setId(3);
-
-        almendras.setImagen("/public/almendras.jpg");
-        nuez.setImagen("/public/nueces.jpg");
-        pistachio.setImagen("/public/pistacho.jpg");
-
-        productos.add(almendras);
-        productos.add(nuez);
-        productos.add(pistachio);
-
-
-        //lacteos
-
-        Producto lecheVegetalAlmendras = new Producto();
-        Producto yogurtGriego = new Producto();
-        lecheVegetalAlmendras.setNombre("Leche vegetal de Almendras");
-        yogurtGriego.setNombre("yogurt Griego");
-        lecheVegetalAlmendras.setCategoria("lacteos");
-        yogurtGriego.setCategoria("lacteos");
-        lecheVegetalAlmendras.setCantidad(100);
-        yogurtGriego.setCantidad(100);
-        lecheVegetalAlmendras.setPrecio(new BigDecimal("150.00"));
-        yogurtGriego.setPrecio(new BigDecimal("300.00"));
-        yogurtGriego.setId(4);
-        lecheVegetalAlmendras.setId(5);
-        lecheVegetalAlmendras.setImagen("/public/almondMilk.jpg");
-        yogurtGriego.setImagen("/public/yogurtGriego.jpg");
-
-        productos.add(lecheVegetalAlmendras);
-        productos.add(yogurtGriego);
-
-
-
-        //Bebidas
-
-        Producto JugoNatural = new Producto();
-        Producto GaseosaNatural = new Producto();
-        JugoNatural.setNombre("Jugo de fruta natural");
-        GaseosaNatural.setNombre("Gaseosa Natural");
-        JugoNatural.setCategoria("bebidas");
-        GaseosaNatural.setCategoria("bebidas");
-        JugoNatural.setCantidad(100);
-        GaseosaNatural.setCantidad(100);
-        JugoNatural.setPrecio(new BigDecimal("150.00"));
-        GaseosaNatural.setPrecio(new BigDecimal("300.00"));
-        JugoNatural.setId(6);
-        GaseosaNatural.setId(7);
-        JugoNatural.setImagen("/public/jugo.jpg");
-        GaseosaNatural.setImagen("/public/gaseosa.jpg");
-
-        productos.add(JugoNatural);
-        productos.add(GaseosaNatural);
-    }
-
-    public List<Producto> obtenerTodos() {
-        return productos;
-    }
-
-    public void guardar(Producto producto) {
-        productos.add(producto);
-    }
-
-    public List<Producto> obtenerProductosPorCategoria(String categoria) {
-        return productos.stream().filter((producto)->producto.getCategoria().equals(categoria)).toList();
-    }
+    public List<Producto> obtenerProductosPorCategoria(long categoria) ;
 }
