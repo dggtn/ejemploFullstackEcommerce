ba433cc07c3c76cddfcbdc031cb6f5f847e90af5
Daniela <dggtn97@gmail.com>
2025-03-29 20:59:29 -0300

    agrego @Entity y @Columns

--- src/main/java/ar/edu/uade/ecommerce/entity/Order.java
+++ src/main/java/ar/edu/uade/ecommerce/entity/Order.java
@@ -1,9 +1,11 @@
 package ar.edu.uade.ecommerce.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "orders")
 public class Order {
 }
