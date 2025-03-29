ded5115fd6b58d836f75812fcfad3653d90faae0
Daniela <dggtn97@gmail.com>
2025-03-29 20:16:06 -0300

    agrego metodos a la interfaz

--- src/main/java/ar/edu/uade/ecommerce/repository/IproductosRepository.java
+++ src/main/java/ar/edu/uade/ecommerce/repository/IproductosRepository.java
@@ -1,19 +1,18 @@
 package ar.edu.uade.ecommerce.repository;
 
 
-import ar.edu.uade.ecommerce.entity.Category;
+
 import ar.edu.uade.ecommerce.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
-public interface IproductosRepository extends JpaRepository<Category, Long> {
+public interface IproductosRepository extends JpaRepository< Producto,Long>{
 
     public List<Producto> obtenerTodos();
 
     public void guardar(Producto producto);
 
-    public List<Producto> obtenerProductosPorCategoria(long categoria) ;
+    public List<Producto> obtenerProductosPorCategoria(Long categoria) ;
 }
