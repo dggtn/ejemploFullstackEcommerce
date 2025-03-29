5fa96755d60ad8633541727f2cc64bad3aac587f
Daniela <dggtn97@gmail.com>
2025-03-29 20:59:49 -0300

    agrego @Entity y @Columns

--- src/main/java/ar/edu/uade/ecommerce/entity/Role.java
+++ src/main/java/ar/edu/uade/ecommerce/entity/Role.java
@@ -1,9 +1,17 @@
 package ar.edu.uade.ecommerce.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String descripcion;
+    
 }
