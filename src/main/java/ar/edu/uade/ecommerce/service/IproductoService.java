92768e12d270501534766f639d69c0185fcd3a1c
Daniela <dggtn97@gmail.com>
2025-03-29 12:35:36 -0300

    creo la interfaz IproductoService coon los metodos que va a usar la clase ProductoService

---
+++ src/main/java/ar/edu/uade/ecommerce/service/IproductoService.java
@@ -0,0 +1,18 @@
+package ar.edu.uade.ecommerce.service;
+
+
+import ar.edu.uade.ecommerce.entity.Producto;
+
+import java.util.List;
+
+public interface IproductoService {
+    public List<Producto> obtenerProductos();
+
+    public void agregarProducto(Producto producto) ;
+
+
+    public List<Producto> obtenerProductosXCategoria(String categoria);
+
+
+}
+
