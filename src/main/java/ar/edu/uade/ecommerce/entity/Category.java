99e3fa7b5b5b958e2f42456ad3f7b96891623998
Daniela <dggtn97@gmail.com>
2025-03-29 19:36:18 -0300

    agrego @Entity a la clase Category

--- src/main/java/ar/edu/uade/ecommerce/entity/Category.java
+++ src/main/java/ar/edu/uade/ecommerce/entity/Category.java
@@ -1,11 +1,16 @@
 package ar.edu.uade.ecommerce.entity;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Entity
 public class Category {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
     private String description;
 }
