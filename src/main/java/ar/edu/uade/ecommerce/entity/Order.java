294115f023e65f1e9c2c8c6b5811baf278782c16
Daniela <dggtn97@gmail.com>
2025-03-29 21:04:09 -0300

    mapeo lista de orders con Id de usuario para mostrar la lista de ordenes

--- src/main/java/ar/edu/uade/ecommerce/entity/Order.java
+++ src/main/java/ar/edu/uade/ecommerce/entity/Order.java
@@ -1,11 +1,17 @@
 package ar.edu.uade.ecommerce.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long count;
+
 }
