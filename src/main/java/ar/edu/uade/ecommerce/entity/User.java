736defabb4263cf274a976feee04345b822714cd
Daniela <dggtn97@gmail.com>
2025-03-29 21:00:03 -0300

    agrego @Entity y @Columns

--- src/main/java/ar/edu/uade/ecommerce/entity/User.java
+++ src/main/java/ar/edu/uade/ecommerce/entity/User.java
@@ -1,10 +1,24 @@
 package ar.edu.uade.ecommerce.entity;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String name;
+
+
+    @Column
+    private String surname;
+
 }
